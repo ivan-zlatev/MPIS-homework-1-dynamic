67d00b94ea3b1bac65cd2a59bf502f39ca0dd504
ivan-zlatev <ivanzlatev90@gmail.com>
2014-03-18 19:12:35 +0200

    final draft

--- dynamic_counter.vhd
+++ dynamic_counter.vhd
@@ -16,30 +16,43 @@ architecture Behavioral of dynamic_counter is
 signal temp : unsigned (4*N-1 downto 0);
 signal c : std_logic_vector(N-1 downto 0);
 begin
-F : for i in 1 to N generate
+Ix : for i in 1 to N generate -- generate first and last instance
 	begin
-	F0 : if ( i = 1 ) generate
+	I0 : if ( i = 1 and i < N ) generate -- generate first instance if N > 1
 		begin U0 : entity work.bcd_counter port map    
 			(clock => clock,
 			reset => reset,
-         q => temp(4*i-1 downto 4*i-4),
-			carry => c(i-1));
-	end generate F0;
-	F1 : if ( i > 1 and i /= N ) generate
-		begin U1 : entity work.bcd_counter port map   
-			(clock => c(i-2),
+         q => temp(3 downto 0),
+			carry => c(0));
+	end generate I0;
+	IL : if ( i = N and i > 1 ) generate -- generate last instance if N > 1
+		begin UL : entity work.bcd_counter port map     
+			(clock => c(N-2),
 			reset => reset,
-         q => temp(4*i-1 downto 4*i-4),
-			carry => c(i-1));
-	end generate F1;
-	F2 : if ( i = N ) generate
-		begin U2 : entity work.bcd_counter port map     
-			(clock => c(i-2),
+			q => temp(4*N-1 downto 4*N-4),
+			carry => c(N-1));
+	end generate IL;
+	instance : if ( i = 1 and i = N ) generate -- generate only one instance if N = 1
+		begin U : entity work.bcd_counter port map
+			(clock => clock,
 			reset => reset,
-			q => temp(4*i-1 downto 4*i-4),
-			carry => c(i-1));
-	end generate F2;
-end generate F;
+			q => temp(3 downto 0),
+			carry => c(0));
+	end generate instance;
+end generate Ix;
+
+Iy: for j in 2 to N generate -- generate middle instances if N > 2
+	begin
+	I : if ( j > 1 and j < N ) generate
+		begin
+		Ux : entity work.bcd_counter port map   
+			(clock => c(j-2),
+			reset => reset,
+         q => temp(4*j-1 downto 4*j-4),
+			carry => c(j-1));
+	end generate I;
+end generate Iy;
+
 q <= temp;
-carry <= c(2);
+carry <= c(N-1);
 end Behavioral;
